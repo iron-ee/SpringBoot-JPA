666e20a14d25c8c9bae19bb325b5fd389206fac2
irone <doyeck@naver.com>
2021-03-05 00:12:48 +0900

    JPA 기초 공부(RestController)

---
+++ src/main/java/com/cos/myjpa/Myjpa2Application.java
@@ -0,0 +1,13 @@
+package com.cos.myjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myjpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myjpa2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/cos/myjpa/exhandler/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.cos.myjpa.exhandler;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cos.myjpa.web.dto.CommonRespDto;
+
+@RestController		// 데이터를 리턴할 수 있음.
+@ControllerAdvice	// 모든 익셉션을 낚아채준다.
+public class GlobalExceptionHandler {
+
+	// 그 중 DataIntegrityViolationException 이 발생하면 해당 함수 실행됨.
+	@ExceptionHandler(value = DataIntegrityViolationException.class)
+	public CommonRespDto<?> dataIntegrityViolation(Exception e) {
+		return new CommonRespDto<>(-1, e.getMessage(), null);
+	}
+	
+	// 그 중 IllegalArgumentException 이 발생하면 해당 함수 실행됨.
+	@ExceptionHandler(value = IllegalArgumentException.class)
+	public CommonRespDto<?> illegalArgument(Exception e) {
+		return new CommonRespDto<>(-1, e.getMessage(), null);
+	}
+	
+	// 그 중 IllegalArgumentException 이 발생하면 해당 함수 실행됨.
+		@ExceptionHandler(value = EmptyResultDataAccessException.class)
+		public CommonRespDto<?> EmptyResultDataAccessException(Exception e) {
+			return new CommonRespDto<>(-1, e.getMessage(), null);
+		}
+}
